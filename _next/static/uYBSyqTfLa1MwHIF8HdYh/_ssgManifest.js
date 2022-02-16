c1188548652aeffdf3918f9a234643b778d1f147
OzymandiasTheGreat <OzymandiasTheGreat@users.noreply.github.com>
2022-02-16 07:15:49 +0000

    deploy: f73a1136c2456aa3e8286a1db2970dcc1f00d474

--- _next/static/uYBSyqTfLa1MwHIF8HdYh/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fposts","\u002Ftag","\u002F","\u002Fposts\u002F[year]\u002F[month]\u002F[day]\u002F[slug]","\u002Fposts\u002F[year]\u002F[month]\u002F[day]","\u002Ftag\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
