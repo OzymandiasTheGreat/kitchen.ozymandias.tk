4e775bdff938d434193ba4c0359e6382b93f0b4a
Ozymandias <tomas.rav@gmail.com>
2022-03-01 16:57:53 +0200

    Better theming for header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-import { Animated, Image, Text, View, ViewStyle } from "react-native";
+import {
+	useColorScheme,
+	useWindowDimensions,
+	Animated,
+	Image,
+	Text,
+	View,
+	ViewStyle,
+} from "react-native";
 import { A, Header, P } from "@expo/html-elements";
 import {
 	useSelectedLanguage,
@@ -8,8 +16,17 @@ import {
 	useTranslation,
 } from "next-export-i18n";
 import { translations } from "../../i18n/index";
-import useTheme from "../pages/_theme";
 import { randInt } from "../util/random";
+import {
+	COLOR_TEXT_BG_DARK,
+	COLOR_TEXT_BG_LIGHT,
+	COLOR_TEXT_FG_DARK,
+	COLOR_TEXT_FG_LIGHT,
+	FONT_HEADING,
+	setOpacity,
+	STYLE_HEADING,
+} from "../theme";
+import useTheme from "../pages/_theme";
 
 const languages = Object.keys(translations);
 const flags: Record<string, string> = {
@@ -19,28 +36,40 @@ const flags: Record<string, string> = {
 const AnimatedHeader = Animated.createAnimatedComponent(Header);
 
 const Profile: React.FC<{ visible: boolean }> = ({ visible }) => {
-	const [index] = useState(randInt(1, 10));
 	const { t } = useTranslation();
-	const theme = useTheme();
+	const scheme = useColorScheme();
+	const [dark, setDark] = useState(false);
+	const [index] = useState(randInt(1, 10));
+
+	useEffect(() => setDark(scheme === "dark"), [scheme]);
 
 	return (
-		<View style={[theme?.profile.container]}>
+		<View style={{ flexDirection: "row", alignItems: "center" }}>
 			{/* eslint-disable-next-line jsx-a11y/alt-text */}
 			<Image
 				source={{
 					uri: `/profile/${index}.jpg`,
 				}}
-				style={[
-					theme?.profile.image,
-					,
-					{ display: visible ? "flex" : "none" },
-				]}></Image>
+				style={{
+					display: visible ? "flex" : "none",
+					width: 70,
+					height: 70,
+					borderColor: dark
+						? COLOR_TEXT_FG_DARK
+						: COLOR_TEXT_FG_LIGHT,
+					borderRadius: "50%" as any,
+					borderWidth: 5,
+					marginRight: 15,
+				}}></Image>
 			<P
 				style={[
-					theme?.text.heading,
-					theme?.profile.text,
-					,
-					{ display: visible ? "flex" : "none" },
+					STYLE_HEADING,
+					{
+						display: visible ? "flex" : "none",
+						color: dark ? COLOR_TEXT_FG_DARK : COLOR_TEXT_FG_LIGHT,
+						fontSize: 20,
+						textAlignVertical: "center",
+					},
 				]}>
 				{t("site.tagline")}
 			</P>
@@ -52,28 +81,28 @@ const BlogHeader: React.FC<{
 	opaque: boolean;
 	style?: ViewStyle;
 }> = ({ opaque, style }) => {
-	const theme = useTheme();
+	const scheme = useColorScheme();
+	const [dark, setDark] = useState(false);
+	const { width } = useWindowDimensions();
 	const backgroundAnimation = useRef(new Animated.Value(0)).current;
 	const backgroundColor = backgroundAnimation.interpolate({
 		inputRange: [0, 1],
 		outputRange: [
-			theme?.header.transparent.backgroundColor as string,
-			theme?.header.opaque.backgroundColor as string,
+			setOpacity(dark ? COLOR_TEXT_BG_DARK : COLOR_TEXT_BG_LIGHT, 0.3),
+			dark ? COLOR_TEXT_BG_DARK : COLOR_TEXT_BG_LIGHT,
 		],
 	});
 	const heightAnimation = useRef(new Animated.Value(0)).current;
 	const height = heightAnimation.interpolate({
 		inputRange: [0, 1],
-		outputRange: [
-			theme?.header.transparent.height as number,
-			theme?.header.opaque.height as number,
-		],
+		outputRange: [40, 80],
 	});
 	const { lang } = useSelectedLanguage();
 	const { t } = useTranslation();
 	const [query] = useLanguageQuery();
 	const params = new URLSearchParams({ lang }).toString();
 
+	useEffect(() => setDark(scheme === "dark"), [scheme]);
 	useEffect(
 		() => {
 			Animated.timing(backgroundAnimation, {
@@ -94,53 +123,106 @@ const BlogHeader: React.FC<{
 	return (
 		<AnimatedHeader
 			style={[
-				theme?.header.style,
+				{
+					flexDirection: "row",
+					alignItems: "center",
+					justifyContent: "space-between",
+					paddingVertical: 3,
+					paddingHorizontal: 20,
+					marginBottom: 20,
+				},
 				style,
 				{
 					backgroundColor,
 					height,
 				},
 			]}>
-			<Profile visible={opaque}></Profile>
-			<View style={[theme?.header.inner]}>
+			<Profile visible={opaque && width > 800}></Profile>
+			<View
+				style={{
+					flexDirection: "row",
+					alignItems: "center",
+					justifyContent: "space-between",
+					width: 330,
+				}}>
 				<Link passHref href={{ pathname: "/", query: { lang } }}>
 					<A>
-						<View style={[theme?.header.link]}>
+						<View
+							style={{
+								flexDirection: "row",
+								alignItems: "center",
+							}}>
 							{/* eslint-disable-next-line jsx-a11y/alt-text */}
 							<Image
 								source={{
 									uri: "/favicon.svg",
 								}}
-								style={[
-									theme?.header.icon,
-									{ marginRight: 5 },
-								]}
+								style={{
+									width: 24,
+									height: 24,
+									resizeMode: "contain",
+									marginRight: 5,
+								}}
 							/>
 							<Text
-								style={[
+								style={
 									opaque
-										? theme?.header.textOpaque
-										: theme?.header.textTransparent,
-								]}>
+										? {
+												color: dark
+													? COLOR_TEXT_FG_DARK
+													: COLOR_TEXT_FG_LIGHT,
+												fontFamily: FONT_HEADING,
+												fontSize: 22,
+												maxWidth: 180,
+										  }
+										: {
+												color: dark
+													? COLOR_TEXT_FG_DARK
+													: COLOR_TEXT_FG_LIGHT,
+												fontFamily: FONT_HEADING,
+												fontSize: 17,
+												maxWidth: 180,
+										  }
+								}>
 								{t("site.title")}
 							</Text>
 						</View>
 					</A>
 				</Link>
 				<A href={`https://tomasrav.me/?${params}`}>
-					<View style={[theme?.header.link]}>
+					<View
+						style={{ flexDirection: "row", alignItems: "center" }}>
 						{/* eslint-disable-next-line jsx-a11y/alt-text */}
 						<Image
 							source={{
 								uri: "/main_logo.svg",
 							}}
-							style={[theme?.header.icon]}></Image>
+							style={{
+								width: 24,
+								height: 24,
+								resizeMode: "contain",
+								marginRight: 5,
+							}}></Image>
 						<Text
-							style={[
+							style={
 								opaque
-									? theme?.header.textOpaque
-									: theme?.header.textTransparent,
-							]}>
+									? {
+											color: dark
+												? COLOR_TEXT_FG_DARK
+												: COLOR_TEXT_FG_LIGHT,
+											fontFamily: FONT_HEADING,
+											fontSize: 22,
+											maxWidth: 180,
+									  }
+									: {
+											color: dark
+												? COLOR_TEXT_FG_DARK
+												: COLOR_TEXT_FG_LIGHT,
+											fontFamily: FONT_HEADING,
+											fontSize: 17,
+											maxWidth: 180,
+									  }
+							}>
 							{t("site.main")}
 						</Text>
 					</View>
@@ -153,13 +235,10 @@ const BlogHeader: React.FC<{
 							passHref
 							href={{ query: { ...query, lang } }}>
 							<A
-								style={[
-									theme?.header.textOpaque,
-									{
-										fontSize: theme?.header.icon.height,
-										marginLeft: 5,
-									},
-								]}>
+								style={{
+									fontSize: 24,
+									marginLeft: 5,
+								}}>
 								{flags[lang]}
 							</A>
 						</Link>
--- src/pages/[[...page]].tsx
+++ src/pages/[[...page]].tsx
@@ -55,7 +55,7 @@ const App: React.FC<{
 				}}
 				scrollEventThrottle={100}
 				stickyHeaderIndices={[0]}>
-				<Header opaque={header} />
+				<Header opaque={header} style={{ width: "100%" }} />
 				<Masonry
 					data={posts}
 					renderItem={renderItem}
